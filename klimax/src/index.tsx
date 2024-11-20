261482ac81b77c2d7abb37bcd77b61aee43f93ce
Kesley Soares <kesleysoares@discente.ufg.br>
2024-11-20 12:48:04 +0000

    tentativa 2

--- klimax/src/index.tsx
+++ klimax/src/index.tsx
@@ -9,7 +9,9 @@ const root = ReactDOM.createRoot(rootElement);
 document.body.style.backgroundColor = "#222222"; /*Cor de Fundo do documento*/
 
 root.render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>
 );
 
 reportWebVitals();
