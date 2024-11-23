04ef6d44795f57eec58e1757eb970e5f4c376231
Kesley Soares <kesleysoares@discente.ufg.br>
2024-11-23 16:05:39 +0000

    mudança de responsividade 1 - cards de funções

--- klimax/src/elements/maincontent/functionscards/functionscards.tsx
+++ klimax/src/elements/maincontent/functionscards/functionscards.tsx
@@ -1,14 +1,65 @@
 import React from "react";
-import Similarfilms from "./similarfilms/similarfilms";
-import Filteredfilms from "./filteredfilms/filteredfilms";
-import Randomfilms from "./randomfilms/randomfilms";
+import './functionscards.css';
 
 function Functionscards() {
   return (
-    <div>
-      <Randomfilms />
-      <Filteredfilms />
-      <Similarfilms />
+    <div className="functionscards">
+       <div className="cards">
+    <img src="https://i.ibb.co/LQkx8Q4/imagem-fundo.png" className="imagemdefundo" />
+      <img
+        src="https://i.ibb.co/k0C2H34/Basedfilms-icon.png"
+        alt="Filmes Similares"
+        className="iconst"
+      />
+      <div className="titulo">
+        <i >
+          <p>Recomendação por Filmes Similares</p>
+        </i>
+      </div>
+      <div className="descricao">
+        <i >
+          Use filmes que você ama para achar filmes que você vai Adorar.
+        </i>
+      </div>
+    </div>
+      <div className="cards">
+      <img src="https://i.ibb.co/LQkx8Q4/imagem-fundo.png" className="imagemdefundo" />
+      <img
+        src="https://i.ibb.co/L8gDt55/Filtros-icon.png"
+        alt="Semelhante aos filmes selecionados"
+        className="icon"
+      />
+      <div className="titulo">
+        <i >
+          <p>Filtre Seus Gostos</p>
+        </i>
+      </div>
+      <div className="descricao">
+        <i >
+          Adicione filtros para encontrar filmes do jeito que você gosta!
+        </i>
+      </div>
+    </div>
+    <div className="cards">
+      <img src="https://i.ibb.co/LQkx8Q4/imagem-fundo.png" className="imagemdefundo" />
+      <img
+        src="https://i.ibb.co/7GtKkQs/aleatorio-icon.png"
+        alt="Semelhante aos filmes selecionados"
+        className="icon"
+      />
+      <div className="titulo">
+        <i >
+          <p>
+            Recomendação <br /> Aleatória
+          </p>
+        </i>
+      </div>
+      <div className="descricao">
+        <i >
+          Sua oportunidade de descobrir novos filmes surpreedentes.
+        </i>
+      </div>
+    </div>
     </div>
   );
 }
